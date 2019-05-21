254e4c9f5fe18e52fa9fec5fc46e1af7d4281a83
frobian21@yahoo.co.uk <frobian21@yahoo.co.uk>
2019-05-21 15:22:46 +0100

    unit tests written

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -5,22 +5,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.Account;
 
 public class Service {
-	private static int accountNumber=0;
+	private static int accountNumber = 0;
+
 	public static void addAccount(Map<Integer, Account> accMap, Account acc) {
 		acc.setAccountNumber(++accountNumber);
 		accMap.put(accountNumber, acc);
 	}
+
 	public static Account retrieveAccount(Map<Integer, Account> accMap, int accNo) {
 		return accMap.get(accNo);
 	}
+
 	public static void convertMap(Map<Integer, Account> accMap) {
-    ObjectMapper mapper = new ObjectMapper();
-    try {
-    	System.out.println(mapper.writeValueAsString(accMap));
-    	mapper.writeValue(System.out, accMap);
-    } catch (Exception e) {
-        e.printStackTrace();
-    }    
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			// System.out.println(mapper.writeValueAsString(accMap));
+			mapper.writeValue(System.out, accMap);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static int countName(String name) {
+		return 0;
 	}
 
 }
---
+++ src/test/java/testing/accountTests.java
@@ -0,0 +1,32 @@
+package testing;
+
+import repo.AccountManager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import domain.Account;
+import service.Service;
+
+public class accountTests {
+	AccountManager accounts;
+	
+	@Before
+	public void init() {
+		accounts = new AccountManager();
+	}
+	@Test
+	public void test1() {
+		Service.addAccount(accounts.getAccounts(), new Account("name", "surname"));
+		Service.addAccount(accounts.getAccounts(), new Account("name", "surname"));
+		assertEquals("count not 2", Service.countName("name"), 2);
+	}	
+	@Test
+	public void test2() {
+		Service.addAccount(accounts.getAccounts(), new Account("name", "surname"));
+		assertEquals("count not 2", Service.countName("name"), 1);
+	}
+	
+}
