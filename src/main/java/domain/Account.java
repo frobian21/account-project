d48b9e592fe136397ed6fc3dbb6ffab3a9db6cb1
frobian21@yahoo.co.uk <frobian21@yahoo.co.uk>
2019-05-22 14:16:09 +0100

    api working

--- src/main/java/domain/Account.java
+++ src/main/java/domain/Account.java
@@ -1,34 +1,48 @@
 package domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "accounts")
 public class Account {
-	private String firstName;
-	private String lastName;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int accountNumber;
-	
-	public Account() {;
+	@Column(length = 30)
+	private String firstName;
+	@Column(length = 30)
+	private String lastName;
+
+	public Account() {
 	}
 
 	public Account(String firstName, String surname) {
-		this.firstName=firstName;
-		lastName=surname;
+		this.firstName = firstName;
+		lastName = surname;
 	}
-	public String getFirstName(){
+
+	public String getFirstName() {
 		return firstName;
 	}
-	public String getLastName(){
+
+	public String getLastName() {
 		return lastName;
 	}
-	public int getAccountNumber(){
+
+	public int getAccountNumber() {
 		return accountNumber;
 	}
-	public void setFirstName(String name){
-		firstName=name;
+
+	public void setFirstName(String name) {
+		firstName = name;
 	}
-	public void setLastName(String name){
-		lastName=name;
+
+	public void setLastName(String name) {
+		lastName = name;
 	}
-	public void setAccountNumber(int number){
-		accountNumber=number;
+
+	public void setAccountNumber(int number) {
+		accountNumber = number;
 	}
-	
+
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into accounts (accountnumber, firstname, lastname) values(1,'John','Doe');
+insert into accounts (accountnumber, firstname, lastname) values(2,'Jane','Doe');
